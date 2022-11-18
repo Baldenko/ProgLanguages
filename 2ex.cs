e66ab8810d8d9c5fa541d17f89d09f2a054c2483
Baldenko <Bratofsister@gmail.com>
2022-11-18 19:38:24 +0300

    Второе задание

---
+++ 2ex.cs
@@ -0,0 +1,21 @@
+﻿// EX 2 Напишите программу, которая на вход принимает два числа и выдаёт, какое число 
+Console.WriteLine ("Напишите первое число");
+
+string FirstNumber = Console.ReadLine() ?? "";
+
+int number1 = int.Parse(FirstNumber);
+
+Console.WriteLine ("Напишите второе число");
+
+string SecondNumber = Console.ReadLine() ?? "";
+
+int number2 = int.Parse(SecondNumber);
+
+if(number1 > number2)
+{
+    Console.WriteLine($"Число {number1} больше числа {number2}");
+}
+else
+{
+    Console.WriteLine($"Число {number2} больше числа {number1}");
+}
